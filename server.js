3dbe46114001f0c88712a0393c6063c47f1049f2
Llovind <lovindfirdaus@gmail.com>
2025-07-05 02:00:37 +0700

    perbaikan typo pada server.js

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.get('/', (req, res) => {
 app.post('/send', async (req, res) => {
   const { name, email, message } = req.body;
 
-  const transporter = nodemailer.createTransporter({
+  const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
       user: process.env.EMAIL_USER,
